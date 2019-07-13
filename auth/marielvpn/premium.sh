50719ed3d9158914591a494a9b30b5e44fa2100d
Rey Omayan <30548067+omayanrey@users.noreply.github.com>
2019-07-13 14:43:18 +0800

    Update premium.sh

--- auth/marielvpn/premium.sh
+++ auth/marielvpn/premium.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+. /etc/openvpn/script/config.sh
 
+
+##PREMIUM##
+PRE="users.user_name='$username' AND users.auth_vpn='$password' AND users.is_validated=1 AND users.is_freeze=0 AND users.is_active=1 AND users.is_ban=0 AND users.duration > 0"
+
+##VIP##
+VIP="users.user_name='$username' AND users.auth_vpn='$password' AND users.is_validated=1 AND users.is_freeze=0 AND users.is_active=1 AND users.is_ban=0 AND users.vip_duration > 0"
+
+##Private##
+PRIV="users.user_name='$username' AND users.auth_vpn='$password' AND users.is_validated=1 AND users.is_freeze=0 AND users.is_active=1 AND users.is_ban=0 AND users.private_duration > 0"
+
+Query="SELECT users.user_name FROM users WHERE $PRE OR $VIP OR $PRIV"
+user_name=`mysql -u $USER -p$PASS -D $DB -h $HOST --skip-column-name -e "$Query"`
+
+[ "$user_name" != '' ] && [ "$user_name" = "$username" ] && echo "user : $username" && echo 'authentication ok.' && exit 0 || echo 'authentication failed.'; exit 1
